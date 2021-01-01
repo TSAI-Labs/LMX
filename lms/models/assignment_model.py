a93c67b379682be081273cf9b4aa5dda2146e60b
Akshat Jaipuria <akshat.jaipuria504@gmail.com>
2021-01-01 16:23:56 +0530

    database_models_1
    Added model for assignment and modified course and user model

---
+++ lms/models/assignment_model.py
@@ -0,0 +1,16 @@
+# Core Django imports.
+from django.contrib.auth.models import User
+from django.db import models
+from lms.models.course_model import Course
+
+
+class Assignment(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    available_from = models.DateTimeField()
+    available_until = models.DateTimeField()
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL)
+    for_course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    max_grade = models.IntegerField()
+
