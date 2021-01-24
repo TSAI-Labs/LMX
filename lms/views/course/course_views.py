22cb8d7b4324aa76ac0e33fd961d686ac1da42b3
Guru Swaroop <bguruswaroop@gmail.com>
2021-01-24 14:22:11 +0100

    Adding back the db, restrict access to admin and staff for grades view

--- lms/views/course/course_views.py
+++ lms/views/course/course_views.py
@@ -1,14 +1,16 @@
 # Core Django imports.
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render
 from django.views.generic import ListView
-
-# Blog application imports.
-from lms.models.course_model import Course
-from lms.models.profile_model import Profile
-from django.shortcuts import render, Http404, HttpResponse
-
-from django_filters import filterset
 from django_filters.views import FilterView
+from django_tables2 import RequestConfig
+from django_tables2.export.export import TableExport
+from django_tables2.export.views import ExportMixin
 from django_tables2.views import SingleTableMixin
+
+from lms.models.assignment_model import StudentAssignment
+from lms.models.course_model import Course
+from lms.tables import StudentAssignmentTable, StudentAssignmentFilter
 
 
 class CourseListView(ListView):
@@ -17,23 +19,24 @@ class CourseListView(ListView):
     template_name = "lms/course/home.html"
 
 
-
-#dummy model 
-from django_tables2 import SingleTableView, RequestConfig
-from django_tables2.export.views import ExportMixin
-
-from django_filters.views import FilterView
-from django_tables2.views import SingleTableMixin
-from django_tables2.export.export import TableExport
-from lms.tables import StudentAssignmentTable, StudentAssignmentFilter
-from lms.models.assignment_model import StudentAssignment
-
-class GradeBookCourseView(ExportMixin, SingleTableMixin, FilterView):
+class GradeBookCourseView(LoginRequiredMixin, UserPassesTestMixin, ExportMixin, SingleTableMixin, FilterView):
     model = StudentAssignment
     table_class = StudentAssignmentTable
-    
+
     template_name = 'lms/course/gradebook/course_gradebook.html'
-    filterset_class  = StudentAssignmentFilter
+    filterset_class = StudentAssignmentFilter
+
+    # Restrict access to only course user (teacher) and admin
+    def test_func(self):
+        if self.request.user.role.is_admin:
+            return True
+        elif self.request.user.role.is_teacher:
+            return True
+        elif self.request.user.role.is_teaching_assistant:
+            return True
+        # elif self.request.user == self.get_object().user:
+        #     return True
+        return False
 
 
 # download csv file (django_tables2 method)
@@ -50,8 +53,3 @@ def table_download(request):
     return render(request, "lms/course/gradebook/course_gradebook_export.html", {
         "table": table
     })
-
-
-
-
-
